02ef8030190bd6b50df45dcedd4d7a7c4b90e52e
Shannon Ashford <email.sa.coder@gmail.com>
2025-02-15 23:16:56 -0500

    Updated original code to use an Inline If Statement

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ORIGINAL CODE - has been commented out
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+                //response = answer + " is less than nine";
+            //}
+            //else
+            //{
+                //response = answer + " is greater than or equal to nine";
+            //}
+
+
+            /* NOTE:  The original code didn't "run" in VSS.  I tried to update the .NET version, but I am
+             * not sure the update was properly applied.  So, this file displays the answer, but
+             * the answer is not operable.*/
+
+            //Convert existing code to "Inline If" statement:
+
+            var answer = 4;
+
+            var response = (answer < 9) ? answer + " is less than nine" : answer + " is greater than or equal to nine";
+
+            Console.WriteLine(response);
+              
+            
+
         }
     }
 }
